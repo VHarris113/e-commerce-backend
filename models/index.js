71dc3f1b796f09fdfd873e4edc1ccf61d34af7b0
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-21 20:10:33 -0400

    editing my foreign keys

--- models/index.js
+++ models/index.js
@@ -7,13 +7,11 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 })
 
 // Products belongToMany Tags (through ProductTag)
