892ccc1bdb6645b8525595b2b5ad68bc07578dfa
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-22 18:15:18 -0400

    attempting to fix the bug within the product routing

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,17 +42,14 @@ router.get('/:id', (req, res) => {
   });
 });
 
-// create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tag_id
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
