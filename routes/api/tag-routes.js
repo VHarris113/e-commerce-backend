55246f6a367740dffee87023d2aa6689b14231e8
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-22 17:17:51 -0400

    adding post and delete part of route to tagRoute.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,20 +1,45 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product
+    }
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product
+    }
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
