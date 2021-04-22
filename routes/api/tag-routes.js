2746691e9a35cedadc6c21591c59821ed0accb97
Veronica Harris <veronica.n.harris92@gmail.com>
2021-04-22 17:23:32 -0400

    added the final route to tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -65,6 +65,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if(!tagData) {
+      res.status(404).json({message: "Invalid tag id. Please try again."});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
